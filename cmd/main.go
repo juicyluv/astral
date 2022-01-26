e9bdecd277ae1ec8694c9a7fb92295a71f75a8cb
ellywynn <canttrymybest@gmail.com>
2022-01-27 01:26:10 +0300

    dependency injection, connected to postgres

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 
+	"github.com/jackc/pgx/v4"
 	"github.com/juicyluv/astral/configs"
 	"github.com/juicyluv/astral/internal/server"
+	"github.com/juicyluv/astral/internal/store/sql"
 	"go.uber.org/zap"
 )
 
@@ -16,6 +19,7 @@ var (
 func main() {
 	flag.Parse()
 
+	// Logger initialization
 	prodLogger, err := zap.NewProduction()
 	if err != nil {
 		log.Fatal(err)
@@ -23,14 +27,31 @@ func main() {
 	defer prodLogger.Sync()
 	logger := prodLogger.Sugar()
 
+	// Load config files
 	if err := configs.LoadConfigs(*configPath); err != nil {
 		logger.Fatal(err)
 	}
 
+	// Create config instance
 	config := server.NewConfig(*configPath)
 
-	server := server.NewServer(&config, logger)
+	// Create database connection
+	conn, err := pgx.Connect(context.Background(), config.DbDSN)
+	if err != nil {
+		logger.Fatal(err)
+	}
 
+	// Try to connect to database
+	if err = conn.Ping(context.Background()); err != nil {
+		logger.Fatal(err)
+	}
+	logger.Info("connected to database")
+
+	store := sql.NewPostgres(conn)
+
+	server := server.NewServer(&config, logger, store)
+
+	// Run the server
 	if err := server.Run(); err != nil {
 		logger.Fatal(err)
 	}
