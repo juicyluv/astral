b56bd6ffa614105561ec9bb6bb941fb24cf3dc3b
ellywynn <canttrymybest@gmail.com>
2022-01-27 01:28:04 +0300

    postgres

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,6 +3,8 @@ package server
 import (
 	"net/http"
 
+	"github.com/juicyluv/astral/internal/handler"
+	"github.com/juicyluv/astral/internal/store"
 	"go.uber.org/zap"
 )
 
@@ -10,17 +12,20 @@ type Server struct {
 	server *http.Server
 	cfg    *Config
 	logger *zap.SugaredLogger
+	db     store.Store
 }
 
-func NewServer(cfg *Config, logger *zap.SugaredLogger) *Server {
+func NewServer(cfg *Config, logger *zap.SugaredLogger, store store.Store) *Server {
 	return &Server{
 		cfg:    cfg,
 		logger: logger,
+		db:     store,
 		server: &http.Server{
 			Addr:           cfg.Port,
 			WriteTimeout:   cfg.WriteTimeout,
 			ReadTimeout:    cfg.ReadTimeout,
 			MaxHeaderBytes: cfg.MaxHeaderBytes,
+			Handler:        handler.NewHandler().GetRouter(),
 		},
 	}
 }
