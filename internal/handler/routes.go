e3f73fae7d4ba124ed6b6df4c911eaf2c4a1476b
ellywynn <canttrymybest@gmail.com>
2022-01-27 22:12:31 +0300

    added user routes

--- internal/handler/routes.go
+++ internal/handler/routes.go
@@ -4,4 +4,8 @@ import "net/http"
 
 func (h *Handler) initRoutes() {
 	h.router.HandlerFunc(http.MethodGet, "/api/health", h.health)
+	h.router.HandlerFunc(http.MethodPost, "/api/user", h.createUser)
+	h.router.HandlerFunc(http.MethodGet, "/api/user", h.listUser)
+	h.router.HandlerFunc(http.MethodGet, "/api/user/:id", h.getUser)
+	h.router.HandlerFunc(http.MethodDelete, "/api/user/:id", h.deleteUser)
 }
