681fcf8cf0927e117f3e2676454110c963225738
ellywynn <canttrymybest@gmail.com>
2022-01-27 22:14:21 +0300

    added post interface

--- internal/store/repository.go
+++ internal/store/repository.go
@@ -1,11 +1,25 @@
 package store
 
-import "github.com/juicyluv/astral/internal/model"
+import (
+	"context"
+
+	"github.com/juicyluv/astral/internal/model"
+)
 
 type UserRepository interface {
-	Create(*model.User) (int, error)
-	Find(int) (*model.User, error)
-	FindByEmail(string) (*model.User, error)
-	Update(*model.UpdateUserDto) error
-	Delete(int) error
+	Create(context.Context, *model.User) (int, error)
+	FindAll(context.Context) ([]model.User, error)
+	FindById(context.Context, int) (*model.User, error)
+	FindByEmail(context.Context, string) (*model.User, error)
+	Update(context.Context, *model.UpdateUserDto) error
+	Delete(context.Context, int) error
+}
+
+type PostRepository interface {
+	Create(context.Context, *model.Post) (int, error)
+	FindAll(context.Context) ([]model.Post, error)
+	FindById(context.Context, int) (*model.Post, error)
+	FindByEmail(context.Context, string) (*model.Post, error)
+	Update(context.Context, *model.UpdatePostDto) error
+	Delete(context.Context, int) error
 }
