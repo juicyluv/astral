d188bb4f5f46687bc44cb3f570face2431eee936
ellywynn <canttrymybest@gmail.com>
2022-01-28 01:43:04 +0300

    fixed post_id column name

--- migrations/000002_create_post_table.up.sql
+++ migrations/000002_create_post_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS posts(
-    id serial primary key not null,
+    post_id serial primary key not null,
     title text not null,
-    subtitle text not null,
+    content text not null,
     created_at timestamptz not null default now(),
     updated_at timestamptz not null default now(),
     author_id int not null,
