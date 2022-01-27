3b00bbe08dd66dab274cf4c2937e4bc038165458
ellywynn <canttrymybest@gmail.com>
2022-01-28 01:45:23 +0300

    validation fixes

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,15 +1,38 @@
 package model
 
+import (
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+)
+
 type User struct {
 	Id           int    `json:"id"`
 	Username     string `json:"username"`
-	Email        string `json:"email"`
-	RegisteredAt string `json:"registered_at"`
-	Password     string `json:"-"`
+	Email        string `json:"email,omitempty"`
+	RegisteredAt string `json:"registered_at,omitempty"`
+	Password     string `json:"password,omitempty"`
 }
 
 type UpdateUserDto struct {
 	Username *string `json:"username"`
 	Email    *string `json:"email"`
 	Password *string `json:"password"`
+}
+
+func (u *User) Validate() error {
+	return validation.ValidateStruct(
+		u,
+		validation.Field(&u.Username, is.Alphanumeric, validation.Length(3, 20), validation.Required),
+		validation.Field(&u.Email, is.Email, validation.Required),
+		validation.Field(&u.Password, is.Alphanumeric, validation.Required),
+	)
+}
+
+func (u *UpdateUserDto) Validate() error {
+	return validation.ValidateStruct(
+		u,
+		validation.Field(&u.Username, is.Alphanumeric, validation.Length(3, 20)),
+		validation.Field(&u.Email, is.Email),
+		validation.Field(&u.Password, is.Alphanumeric),
+	)
 }
