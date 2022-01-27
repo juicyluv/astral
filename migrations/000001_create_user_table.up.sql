256f8c356c4d6ed843340d86497022dbc4719f68
ellywynn <canttrymybest@gmail.com>
2022-01-28 01:42:21 +0300

    user_id fix

--- migrations/000001_create_user_table.up.sql
+++ migrations/000001_create_user_table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users(
-    id serial primary key not null,
+    user_id serial primary key not null,
     username text not null,
     email text not null,
     registered_at timestamptz not null default now(),
