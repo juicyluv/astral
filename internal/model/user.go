4942a0003346c9570afbd8dba0d4be58324966b3
ellywynn <canttrymybest@gmail.com>
2022-01-27 22:12:53 +0300

    now register date is string

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,18 +1,15 @@
 package model
 
-import "time"
-
 type User struct {
-	Id           int       `json:"id"`
-	Username     string    `json:"username"`
-	Email        string    `json:"email"`
-	RegisteredAt time.Time `json:"registered_at"`
-	Password     string
+	Id           int    `json:"id"`
+	Username     string `json:"username"`
+	Email        string `json:"email"`
+	RegisteredAt string `json:"registered_at"`
+	Password     string `json:"-"`
 }
 
 type UpdateUserDto struct {
-	Username     *string    `json:"username"`
-	Email        *string    `json:"email"`
-	RegisteredAt *time.Time `json:"registered_at"`
-	Password     *string    `json:"password"`
+	Username *string `json:"username"`
+	Email    *string `json:"email"`
+	Password *string `json:"password"`
 }
