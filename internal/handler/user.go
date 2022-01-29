702582b4aa256a012807dadedbff6177c2546689
ellywynn <canttrymybest@gmail.com>
2022-01-29 21:57:03 +0300

    fixes

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -26,6 +26,23 @@ func (h *Handler) createUser(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 
+	found, err := h.store.User().FindByEmail(ctx, user.Email)
+	if err != nil {
+		if err != errNoRows {
+			if found != nil {
+				h.badRequestResponse(w, r, errors.New("email already taken"))
+			} else {
+				h.internalErrorResponse(w, r, err)
+			}
+			return
+		}
+	}
+
+	if err = user.HashPassword(); err != nil {
+		h.internalErrorResponse(w, r, err)
+		return
+	}
+
 	userId, err := h.store.User().Create(ctx, &user)
 	if err != nil {
 		h.internalErrorResponse(w, r, err)
@@ -143,6 +160,42 @@ func (h *Handler) deleteUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	err = sendJSON(w, nil, http.StatusOK, nil)
+	if err != nil {
+		h.internalErrorResponse(w, r, err)
+	}
+}
+
+func (h *Handler) listUserPosts(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	userId, err := readIdParam(r)
+	if err != nil {
+		h.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	_, err = h.store.User().FindById(ctx, userId)
+	if err != nil {
+		if errors.Is(err, errNoRows) {
+			h.errorResponse(w, r, http.StatusBadRequest, "user with this id not found")
+		} else {
+			h.internalErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	posts, err := h.store.Post().FindUserPosts(ctx, userId)
+	if err != nil {
+		if errors.Is(err, errNoRows) {
+			h.recordNotFoundResponse(w, r)
+		} else {
+			h.internalErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	err = sendJSON(w, posts, http.StatusOK, nil)
 	if err != nil {
 		h.internalErrorResponse(w, r, err)
 	}
