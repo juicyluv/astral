1aa4d41c37fcaad21816e63c578e1647a92fe347
ellywynn <canttrymybest@gmail.com>
2022-01-29 21:56:45 +0300

    auth routes

--- internal/handler/routes.go
+++ internal/handler/routes.go
@@ -8,15 +8,18 @@ func (h *Handler) initRoutes() {
 
 	h.router.HandlerFunc(http.MethodGet, "/api/health", h.health)
 
-	h.router.HandlerFunc(http.MethodPost, "/api/user", h.createUser)
-	h.router.HandlerFunc(http.MethodGet, "/api/user", h.listUser)
-	h.router.HandlerFunc(http.MethodGet, "/api/user/:id", h.getUser)
-	h.router.HandlerFunc(http.MethodPut, "/api/user/:id", h.updateUser)
-	h.router.HandlerFunc(http.MethodDelete, "/api/user/:id", h.deleteUser)
+	h.router.HandlerFunc(http.MethodPost, "/api/auth/signin", h.login)
+	h.router.HandlerFunc(http.MethodPost, "/api/auth/signup", h.createUser)
 
-	h.router.HandlerFunc(http.MethodGet, "/api/post", h.listPost)
-	h.router.HandlerFunc(http.MethodPost, "/api/post", h.createPost)
-	h.router.HandlerFunc(http.MethodGet, "/api/post/:id", h.getPost)
-	h.router.HandlerFunc(http.MethodPut, "/api/post/:id", h.updatePost)
-	h.router.HandlerFunc(http.MethodDelete, "/api/post/:id", h.deletePost)
+	h.router.HandlerFunc(http.MethodGet, "/api/users", h.listUser)
+	h.router.HandlerFunc(http.MethodGet, "/api/users/:id", h.getUser)
+	h.router.HandlerFunc(http.MethodPut, "/api/users/:id", h.updateUser)
+	h.router.HandlerFunc(http.MethodDelete, "/api/users/:id", h.deleteUser)
+	h.router.HandlerFunc(http.MethodGet, "/api/users/:id/posts", h.listUserPosts)
+
+	h.router.HandlerFunc(http.MethodGet, "/api/posts", h.listPost)
+	h.router.HandlerFunc(http.MethodPost, "/api/posts", h.createPost)
+	h.router.HandlerFunc(http.MethodGet, "/api/posts/:id", h.getPost)
+	h.router.HandlerFunc(http.MethodPut, "/api/posts/:id", h.updatePost)
+	h.router.HandlerFunc(http.MethodDelete, "/api/posts/:id", h.deletePost)
 }
