a2c865274d789e69ed779ffbab3b4d7141ed6ef6
ellywynn <canttrymybest@gmail.com>
2022-01-29 21:55:40 +0300

    redis added

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"github.com/go-redis/redis/v7"
 	"github.com/juicyluv/astral/internal/store"
 	"github.com/julienschmidt/httprouter"
 	"go.uber.org/zap"
@@ -9,16 +10,18 @@ import (
 type Handler struct {
 	router *httprouter.Router
 	logger *zap.SugaredLogger
+	redis  *redis.Client
 	store  store.Store
 }
 
 type jsonResponse map[string]interface{}
 
-func NewHandler(logger *zap.SugaredLogger, store store.Store) *Handler {
+func NewHandler(logger *zap.SugaredLogger, store store.Store, redis *redis.Client) *Handler {
 	h := &Handler{
 		router: httprouter.New(),
 		logger: logger,
 		store:  store,
+		redis:  redis,
 	}
 
 	h.initRoutes()
