2db0b931975e71d2d7d58cdb977f1326e959d448
ellywynn <canttrymybest@gmail.com>
2022-01-30 18:14:54 +0300

    comments

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -2,11 +2,12 @@ package handler
 
 import "net/http"
 
+// RequireAuth middleware will check if token is presented and if it is valid
 func (h *Handler) RequireAuth(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		err := h.isTokenExpired(r)
 		if err != nil {
-			h.UnauthorizedResponse(w, r)
+			h.unauthorizedResponse(w, r)
 			return
 		}
 
