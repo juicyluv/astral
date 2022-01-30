7c3908bfaaf3fadff89fb64bf212e75baacfb9b8
ellywynn <canttrymybest@gmail.com>
2022-01-30 03:57:59 +0300

    refresh token implementation

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -11,7 +11,9 @@ import (
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/gofrs/uuid"
 	"github.com/juicyluv/astral/internal/model"
+	"github.com/spf13/viper"
 )
 
 func (h *Handler) login(w http.ResponseWriter, r *http.Request) {
@@ -44,7 +46,7 @@ func (h *Handler) login(w http.ResponseWriter, r *http.Request) {
 
 	user.ClearPassword()
 
-	tokens, err := login.CreateToken(user.Id)
+	tokens, err := h.CreateToken(user.Id)
 	if err != nil {
 		h.internalErrorResponse(w, r, err)
 		return
@@ -67,19 +69,88 @@ func (h *Handler) login(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) logout(w http.ResponseWriter, r *http.Request) {
-	token, err := h.GetTokenMetadata(r)
+	token, err := h.getTokenMetadata(r)
 	if err != nil {
 		h.UnauthorizedResponse(w, r)
 		return
 	}
 
-	deleted, err := h.RemoveUserTokenFromCache(token.AccessUuid)
+	deleted, err := h.removeUserTokenFromCache(token.AccessUuid)
 	if err != nil || deleted == 0 {
 		h.internalErrorResponse(w, r, err)
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
+}
+
+func (h *Handler) CreateToken(userId int) (*model.TokenDetails, error) {
+	// Access Token secret key
+	accessSecret := os.Getenv("JWT_SECRET")
+	if accessSecret == "" {
+		accessSecret = "supersecretkey"
+	}
+
+	// Refresh token secret key
+	refreshSecret := os.Getenv("JWT_REFRESH_SECRET")
+	if refreshSecret == "" {
+		refreshSecret = "supersecretkey"
+	}
+
+	// Access and Refresh token exp time
+	tokenExpTimeMinutes := time.Duration(viper.GetInt("auth.tokenExpTime"))
+	refreshExpTimeDays := time.Duration(viper.GetInt("auth.refreshExpTime"))
+
+	// Token details structure
+	td := model.TokenDetails{}
+
+	// Generate access token uuid and exp time
+	td.AtExpires = time.Now().Add(time.Minute * tokenExpTimeMinutes).Unix()
+	accessUuid, err := uuid.NewV4()
+	if err != nil {
+		return nil, err
+	}
+	td.AccessUuid = accessUuid.String()
+
+	// Generate refresh token uuid and exp time
+	td.RtExpires = time.Now().Add(time.Hour * 24 * refreshExpTimeDays).Unix()
+	refreshUuid, err := uuid.NewV4()
+	if err != nil {
+		return nil, err
+	}
+	td.RefreshUuid = refreshUuid.String()
+
+	// Access token payload
+	accessClaims := jwt.MapClaims{}
+	accessClaims["user_id"] = userId
+	accessClaims["access_uuid"] = td.AccessUuid
+	accessClaims["authorized"] = true
+	accessClaims["exp"] = td.AtExpires
+
+	// Generate Access token
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, accessClaims)
+	accessToken, err := at.SignedString([]byte(accessSecret))
+	if err != nil {
+		return nil, err
+	}
+	td.AccessToken = accessToken
+
+	// Refresh token payload
+	refreshClaims := jwt.MapClaims{}
+	refreshClaims["user_id"] = userId
+	refreshClaims["refresh_uuid"] = td.AccessUuid
+	refreshClaims["authorized"] = true
+	refreshClaims["exp"] = td.RtExpires
+
+	// Generate Refresh token
+	rt := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims)
+	refreshToken, err := rt.SignedString([]byte(refreshSecret))
+	if err != nil {
+		return nil, err
+	}
+	td.RefreshToken = refreshToken
+
+	return &td, nil
 }
 
 func (h *Handler) saveTokenInformation(userId int, td *model.TokenDetails) error {
@@ -103,7 +174,7 @@ func (h *Handler) saveTokenInformation(userId int, td *model.TokenDetails) error
 	return nil
 }
 
-func (h *Handler) ExtractToken(r *http.Request) (string, error) {
+func (h *Handler) extractToken(r *http.Request) (string, error) {
 	err := errors.New("invalid authorization header")
 
 	bearer := r.Header.Get("Authorization")
@@ -119,8 +190,8 @@ func (h *Handler) ExtractToken(r *http.Request) (string, error) {
 	return s[1], nil
 }
 
-func (h *Handler) VerifyToken(r *http.Request) (*jwt.Token, error) {
-	tokenString, err := h.ExtractToken(r)
+func (h *Handler) verifyToken(r *http.Request) (*jwt.Token, error) {
+	tokenString, err := h.extractToken(r)
 	if err != nil {
 		return nil, err
 	}
@@ -141,8 +212,8 @@ func (h *Handler) VerifyToken(r *http.Request) (*jwt.Token, error) {
 }
 
 // IsTokenExiped will check whether token valid or has already expired
-func (h *Handler) IsTokenExpired(r *http.Request) error {
-	token, err := h.VerifyToken(r)
+func (h *Handler) isTokenExpired(r *http.Request) error {
+	token, err := h.verifyToken(r)
 	if err != nil {
 		return err
 	}
@@ -154,9 +225,9 @@ func (h *Handler) IsTokenExpired(r *http.Request) error {
 	return nil
 }
 
-// GetTokenMetadata will extract token metadata and return it if there is no error
-func (h *Handler) GetTokenMetadata(r *http.Request) (*model.TokenMetadata, error) {
-	token, err := h.VerifyToken(r)
+// getTokenMetadata will extract token metadata and return it if there is no error
+func (h *Handler) getTokenMetadata(r *http.Request) (*model.TokenMetadata, error) {
+	token, err := h.verifyToken(r)
 	if err != nil {
 		return nil, err
 	}
@@ -184,8 +255,8 @@ func (h *Handler) GetTokenMetadata(r *http.Request) (*model.TokenMetadata, error
 	return nil, err
 }
 
-// FetchTokenDataFromRedis will return the user id depends on given token metadata
-func (h *Handler) FetchTokenDataFromRedis(details *model.TokenMetadata) (int, error) {
+// fetchTokenDataFromRedis will return the user id depends on given token metadata
+func (h *Handler) fetchTokenDataFromRedis(details *model.TokenMetadata) (int, error) {
 	userIdString, err := h.redis.Get(details.AccessUuid).Result()
 	if err != nil {
 		return 0, err
@@ -199,10 +270,99 @@ func (h *Handler) FetchTokenDataFromRedis(details *model.TokenMetadata) (int, er
 	return userId, nil
 }
 
-func (h *Handler) RemoveUserTokenFromCache(uuid string) (int, error) {
+func (h *Handler) removeUserTokenFromCache(uuid string) (int, error) {
 	deleted, err := h.redis.Del(uuid).Result()
 	if err != nil {
 		return 0, err
 	}
 	return int(deleted), nil
+}
+
+func (h *Handler) refreshToken(w http.ResponseWriter, r *http.Request) {
+	type input struct {
+		RefreshToken string `json:"refreshToken"`
+	}
+
+	tokenInput := input{}
+
+	if err := readJSON(w, r, &tokenInput); err != nil {
+		h.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Parse token and make sure that it uses HMAC hash algorithm
+	token, err := jwt.Parse(tokenInput.RefreshToken, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+
+		return []byte(os.Getenv("JWT_REFRESH_SECRET")), nil
+	})
+
+	if err != nil {
+		h.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Check whether token is valid
+	if !token.Valid {
+		h.UnauthorizedResponse(w, r)
+		return
+	}
+
+	// Get token claims and parse it
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		errInvalidToken := errors.New("invalid token")
+
+		// Get refresh UUID and convert the interface to string
+		refreshUuid, ok := claims["refresh_uuid"].(string)
+		if !ok {
+			fmt.Println("uuid")
+			h.errorResponse(w, r, http.StatusUnprocessableEntity, errInvalidToken.Error())
+			return
+		}
+
+		// Get user id from claims and convert it to int
+		userId, err := strconv.ParseUint(fmt.Sprintf("%.f", claims["user_id"]), 10, 64)
+		if err != nil {
+			fmt.Println("user id")
+			h.errorResponse(w, r, http.StatusUnprocessableEntity, errInvalidToken.Error())
+			return
+		}
+
+		// Remove user token from cache
+		deleted, err := h.removeUserTokenFromCache(refreshUuid)
+		if err != nil || deleted == 0 {
+			h.errorResponse(w, r, http.StatusUnprocessableEntity, errInvalidToken.Error())
+			return
+		}
+
+		// Create a new pair of tokens
+		ts, err := h.CreateToken(int(userId))
+		if err != nil {
+			h.errorResponse(w, r, http.StatusForbidden, err.Error())
+			return
+		}
+
+		// Save new pair of tokens in cache
+		err = h.saveTokenInformation(int(userId), ts)
+		if err != nil {
+			h.errorResponse(w, r, http.StatusForbidden, err.Error())
+			return
+		}
+
+		// Return new tokens
+		tokens := jsonResponse{
+			"accessToken":  ts.AccessToken,
+			"refreshToken": ts.RefreshToken,
+		}
+
+		if err := sendJSON(w, tokens, http.StatusOK, nil); err != nil {
+			h.internalErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	h.errorResponse(w, r, http.StatusUnauthorized, "token expired")
 }
