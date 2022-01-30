b7f5cbc858cb15c11c45fd908986dc6f23177f43
ellywynn <canttrymybest@gmail.com>
2022-01-30 18:14:38 +0300

    added request timeout variable

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,10 +1,17 @@
 package handler
 
 import (
+	"time"
+
 	"github.com/go-redis/redis/v7"
 	"github.com/juicyluv/astral/internal/store"
 	"github.com/julienschmidt/httprouter"
+	"github.com/spf13/viper"
 	"go.uber.org/zap"
+)
+
+var (
+	requestTimeout = time.Duration(viper.GetInt("http.requestTimeout")) * time.Second
 )
 
 type Handler struct {
@@ -29,6 +36,7 @@ func NewHandler(logger *zap.SugaredLogger, store store.Store, redis *redis.Clien
 	return h
 }
 
+// GetRouter returns a router instance pointer
 func (h *Handler) GetRouter() *httprouter.Router {
 	return h.router
 }
