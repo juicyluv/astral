e2cada906618ec46e19acab9a356e0d3d84e75f2
ellywynn <canttrymybest@gmail.com>
2022-02-02 03:12:25 +0300

    now mail can send HTML messages

--- internal/mail/mail.go
+++ internal/mail/mail.go
@@ -1,29 +1,45 @@
 package mail
 
 import (
+	"fmt"
 	"net/smtp"
 	"os"
 
 	"github.com/spf13/viper"
 )
 
-func SendEmail(to, message string) error {
+var (
+	MimeHTML = "text/html"
+)
+
+func SendEmail(to, subject, mimeType, body string) error {
 	from := os.Getenv("EMAIL_ADDRESS")
 	password := os.Getenv("EMAIL_PASSWORD")
 	host := viper.GetString("mail.host")
 	port := viper.GetString("mail.port")
 
-	subject := viper.GetString("mail.subject")
+	header := make(map[string]string)
+	header["From"] = from
 
-	msg := "From: " + from + "\n" +
-		"To: " + to + "\n" +
-		"Subject: " + subject + "\n\n" +
-		message
+	header["To"] = to
+	header["Subject"] = subject
+	header["MIME-Version"] = "1.0"
+	header["Content-Type"] = fmt.Sprintf("%s; charset=\"utf-8\"", mimeType)
+	header["Content-Transfer-Encoding"] = "quoted-printable"
+	header["Content-Disposition"] = "inline"
+
+	message := ""
+
+	for key, value := range header {
+		message += fmt.Sprintf("%s: %s\r\n", key, value)
+	}
+
+	message += "\r\n" + body
 
 	receiver := []string{to}
 	sender := host + ":" + port
 
 	auth := smtp.PlainAuth("", from, password, host)
 
-	return smtp.SendMail(sender, auth, from, receiver, []byte(msg))
+	return smtp.SendMail(sender, auth, from, receiver, []byte(message))
 }
