7a19a67b80c3b057e63377b639f1223443e6294c
ellywynn <canttrymybest@gmail.com>
2022-02-02 23:53:25 +0300

    added queue to the handler

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/go-redis/redis/v7"
+	"github.com/juicyluv/astral/internal/queue"
 	"github.com/juicyluv/astral/internal/store"
 	"github.com/julienschmidt/httprouter"
 	"github.com/spf13/viper"
@@ -15,18 +16,20 @@ type Handler struct {
 	logger *zap.SugaredLogger
 	redis  *redis.Client
 	store  store.Store
+	queue  *queue.Queue
 
 	requestTimeout time.Duration
 }
 
 type jsonResponse map[string]interface{}
 
-func NewHandler(logger *zap.SugaredLogger, store store.Store, redis *redis.Client) *Handler {
+func NewHandler(logger *zap.SugaredLogger, store store.Store, redis *redis.Client, queue *queue.Queue) *Handler {
 	h := &Handler{
 		router: httprouter.New(),
 		logger: logger,
 		store:  store,
 		redis:  redis,
+		queue:  queue,
 
 		requestTimeout: time.Duration(viper.GetInt("http.requestTimeout")) * time.Second,
 	}
